e1057d2427d6c5be29a74ef4d1710b24569a0fe2
wisehero <godjw96@gmail.com>
2020-04-28 15:55:04 +0900

    Create 8-8.js

---
+++ ch8/8-8.js
@@ -0,0 +1,17 @@
+// 사람 데이터를 저장하는 클로저를 생성하는 함수
+
+function Person(name, age) {
+    var _name = name;
+    var _age = age;
+    return {
+        getName: function () { return _name; },
+        getAge: function () { return _age; },
+        setAge: function () { _age = x; }
+    };
+}
+
+var person = Person("Tom", 18);
+console.log(person.getName());
+console.log(person.getAge());
+person.setAge(19);
+console.log(person.getAge());
