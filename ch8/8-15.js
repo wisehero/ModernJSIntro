dbeabfc545ed40fe31bde0f224aff42dff79ff32
wisehero <godjw96@gmail.com>
2020-04-30 18:55:51 +0900

    Create 8-15.js

---
+++ ch8/8-15.js
@@ -0,0 +1,18 @@
+// 피보나치 수열을 만드는 함수의 메모이제이션
+
+function memorize(f) {
+    var cache = {};
+    return function (x) {
+        if (cache[x] == undefined) cache[x] = f(x);
+        return cache[x];
+    };
+
+}
+
+var fibonacci = memorize(function (n) {
+    if (n < 2) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+});
+for (var i = 0; i <= 20; i++) {
+    console.log(("   " + i).slice(-2) + ":" + fibonacci(i));
+}
